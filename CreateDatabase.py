fd73a8405cf9ea51293c8510cc7ae70829e623a3
Steven <stevenjfields97@gmail.com>
2020-02-09 15:56:33 -0600

    CreateDatabase.py creates an sqlite3 database.

--- CreateDatabase.py
+++ CreateDatabase.py
@@ -9,8 +9,19 @@ def CreateDB():
   
     # Fighters table
     db.execute('''CREATE TABLE Fighters
-                (Name text, Height text, Weight text, Reach text
-                Stance text, DOB text)''')
+                (FighterID int NOT NULL,
+                Name text, Height text, Weight text,
+                Reach text, Stance text, DOB text)''')
+
+    # Fight table
+    db.execute('''CREATE TABLE Fight
+                (FightID int NOT NULL, Winner int NOT NULL, Loser int NOT NULL,
+                Knockdowns tinyint, Sig_Str_Landed smallint, Sig_Str_Attempted smallint,
+                Total_Str_Landed smallint, Total_Str_Attemped smallint,
+                Takedowns_Landed tinyint, Takedowns_Attempted tinyint,
+                Submissions_Attempted tinyint, Passes tinyint, Reversals tinyint,
+                FOREIGN KEY (Winner) REFERENCES Fighters(FighterID),
+                FOREIGN KEY (Loser) REFERENCES Fighters(FighterID))''')
     
     # Round table
     db.execute('''CREATE TABLE Round
@@ -21,5 +32,6 @@ def CreateDB():
                 Submissions_Attempted tinyint, Passes tinyint, Reversals tinyint,
                 PRIMARY KEY (RoundID),
                 FOREIGN KEY (FightID) REFERENCES Fight(FightID))''')
-    
-    # Fight table
+
+if __name__ == '__main__':
+    CreateDB()
